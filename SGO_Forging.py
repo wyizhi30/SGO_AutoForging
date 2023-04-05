9d45d3b1ce9550e1325f30a82a5ba67e7559e47a
wyizhi30 <wyizhi30@gmail.com>
2023-04-05 16:18:46 +0800

    Forging add user input

--- SGO_Forging.py
+++ SGO_Forging.py
@@ -35,28 +35,37 @@ time.sleep(2)
 #==================選擇裝備類型==================
 equip_type = driver.find_elements(By.CLASS_NAME, "css-0")
 print("--------")
-# equip = input("請輸入武器類型:")       #鍵盤輸入
-equip = "短刀"
+equip_input = input("請輸入武器類型:")       #鍵盤輸入
+# equip = "短刀"
 for i in range(2,len(equip_type)):    
-    if(equip in equip_type[i].text):
+    if(equip_input in equip_type[i].text):
         equip_type[i].click()
         print(equip_type[i].text)
         print("--------")
         break
-time.sleep(2)
+# 新增輸入錯誤反應
+time.sleep(1)
 #==================輸入裝備名稱==================
 name = driver.find_element(By.CLASS_NAME, "chakra-input.css-1p7h7cr")
-# name.send_keys(input("請輸入裝備名稱:"))    #鍵盤輸入
-name.send_key("煎包機械飛升")
-time.sleep(3)
+name.send_keys(input("請輸入裝備名稱:"))    #鍵盤輸入
+# name.send_key("今天下午關服")
+time.sleep(1)
 
-while(1):      #重複直到程式錯誤，無窮迴圈，之後再訂條件
+material_list = driver.find_element(By.TAG_NAME, "tbody")
+material_list = material_list.text.split("\n")
+# list ==> '泥土', '158 沒有像樣的素材時能用來充數的材料', '爐渣', '45 冶煉之後的副產品，不知道是誰隨意傾倒'
+
+while(1):     #重複直到程式錯誤，無窮迴圈，之後再訂條件
     #==================選擇素材==================
-    rabbit_fur = driver.find_element(By.CLASS_NAME, "css-104kv8y")  #選擇標記為紫色的素材
-    for i in range(10):
-        print(rabbit_fur.text)              #目前10個兔皮，可優化成輸入自訂素材
-        rabbit_fur.click()
-    time.sleep(2)
+    for i in range(int(input("請問要使用幾種素材?"))):
+        material_input = input("請輸入第%d種素材(ex:兔皮 5) :" % (i+1))
+        material_input = material_input.split(" ")
+        for i in range(0, len(material_list), 2):
+            if(material_list[i] == material_input[0]):
+                for i in range(int(material_input[1])):
+                    driver.find_element(By.CLASS_NAME, "css-1kt2y43").click()
+                    print(material_input[0])
+        time.sleep(1)
 
     #==================開始鍛造==================
     start_forging = driver.find_element(By.CLASS_NAME, "chakra-button.css-9xrim4")
