7125fa095d6be3351c9203be7a01f91b53ba3a58
wyizhi30 <wyizhi30@gmail.com>
2023-04-05 17:28:31 +0800

    因應裝備種類調整等待時間

--- SGO_Forging.py
+++ SGO_Forging.py
@@ -6,21 +6,22 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from playsound import playsound
 import time
+import re
 
 chrome_options = Options()
 chrome_options.page_load_strategy = 'eager'
 driver = webdriver.Chrome(options=chrome_options)
 # driver = webdriver.Chrome()
 driver.get("https://swordgale.online/")
-
+time.sleep(3)
 #==================登入==================
 email=driver.find_element(By.ID, "e")
-# email.send_keys(input("請輸入電子信箱:"))    #鍵盤輸入
-email.send_keys("wyizhi30@gmail.com")
+email.send_keys(input("請輸入電子信箱:"))    #鍵盤輸入
+# email.send_keys("your@gmail.com")          #記住信箱
 
 password=driver.find_element(By.ID, "p")
-# password.send_keys(input("請輸入密碼:"))    #鍵盤輸入
-password.send_keys("tpjh0410403") 
+password.send_keys(input("請輸入密碼:"))    #鍵盤輸入
+# password.send_keys("your password")        #記住密碼
 
 SignIn=driver.find_element(By.CLASS_NAME, "chakra-button")
 time.sleep(1)
@@ -40,15 +41,17 @@ equip_input = input("請輸入武器類型:")       #鍵盤輸入
 for i in range(2,len(equip_type)):    
     if(equip_input in equip_type[i].text):
         equip_type[i].click()
+        equip_args = re.findall(r"\d+", equip_type[i].text)    #獲取鍛造時間及材料上限，後面判斷會用到
         print(equip_type[i].text)
         print("--------")
         break
-# 新增輸入錯誤反應
+# 後續新增輸入錯誤反應
 time.sleep(1)
+
 #==================輸入裝備名稱==================
 name = driver.find_element(By.CLASS_NAME, "chakra-input.css-1p7h7cr")
 name.send_keys(input("請輸入裝備名稱:"))    #鍵盤輸入
-# name.send_key("今天下午關服")
+# name.send_key("機械煎包飛升")
 time.sleep(1)
 
 material_list = driver.find_element(By.TAG_NAME, "tbody")
@@ -57,7 +60,7 @@ material_list = material_list.text.split("\n")
 
 while(1):     #重複直到程式錯誤，無窮迴圈，之後再訂條件
     #==================選擇素材==================
-    for i in range(int(input("請問要使用幾種素材?"))):
+    for i in range(int(input("材料上限%s，請問要使用幾種素材? " %equip_args[1]))):
         material_input = input("請輸入第%d種素材(ex:兔皮 5) :" % (i+1))
         material_input = material_input.split(" ")
         for i in range(0, len(material_list), 2):
@@ -66,22 +69,27 @@ while(1):     #重複直到程式錯誤，無窮迴圈，之後再訂條件
                     driver.find_element(By.CLASS_NAME, "css-1kt2y43").click()
                     print(material_input[0])
         time.sleep(1)
+    # 增加輸入判斷
 
     #==================開始鍛造==================
     start_forging = driver.find_element(By.CLASS_NAME, "chakra-button.css-9xrim4")
     start_forging.click()
-    print("foring......")
-    time.sleep(3)
+    print("等待時間%s分鐘，foring......" %equip_args[0])
+    time.sleep(1)
 
     #==================鍛造完成==================
     move = driver.find_element(By.TAG_NAME, "body")
     move.send_keys(Keys.HOME)
-    time.sleep(450)         #7分鐘後點擊完成鍛造，更換不同武器要改時間
+    time.sleep(int(equip_args[0]) * 60 + 10)         #7分鐘後點擊完成鍛造，更換不同武器要改時間
     done = driver.find_element(By.CLASS_NAME, "chakra-button.css-1zb9ui")
     done.click()
     print("Done!\n-------")
     time.sleep(2)
 
-playsound("G5SH - (Let Go Of Yourself) ft. Marz23 Official Visualizer.m4a")  #鍛造結束撥放提示音樂
+# playsound("G5SH - (Let Go Of Yourself) ft. Marz23 Official Visualizer.m4a")  #鍛造結束撥放提示音樂
 time.sleep(2)
 driver.quit()
+
+
+
+# 有空再來加圖形化介面
